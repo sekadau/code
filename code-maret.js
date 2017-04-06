983c0d754f85581b256467f01c8ac61c45e4b4d0
sekadau <heriyakop@gmail.com>
2017-04-06 16:46:00 +0700

    Add files via upload

---
+++ code-maret.js
@@ -0,0 +1,10 @@
+﻿//<![CDATA[
+$(window).bind("load resize", function(){
+ var url = location.protocol+'//'+location.host+location.pathname;
+  var containercm_width = $('#container-commentfb').width(); 
+    $('#container-commentfb').html('<div class="fb-comments" ' +
+    'data-href="'+url+'"' +
+    ' width="' + containercm_width + '" data-num-posts="10"></div>');
+    FB.XFBML.parse( ); 
+  });
+//]]>
