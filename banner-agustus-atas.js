d645e7f41d325cd6d7cb40404415cd3f3d7828c4
sekadau <heriyakop@gmail.com>
2017-08-31 20:27:27 +0700

    Update banner-agustus-atas.js

--- banner-agustus-atas.js
+++ banner-agustus-atas.js
@@ -1,6 +1,6 @@
 
 
 
-<img src="https://3.bp.blogspot.com/-2MKcuArq5Uw/WaIBZEujV4I/AAAAAAAANlo/MHHRjxw-kb8VnegXRkBUXLN6zCtuVByhQCLcBGAs/s1600/900x200.jpg" alt="Beriklan" style="width:100%;height:100%;">
+https://3.bp.blogspot.com/-2MKcuArq5Uw/WaIBZEujV4I/AAAAAAAANlo/MHHRjxw-kb8VnegXRkBUXLN6zCtuVByhQCLcBGAs/s1600/900x200.jpg
 
 
